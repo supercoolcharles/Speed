fc059d7579f16056c77dd5556c9c2ce7dd4da47b
GQR726 <223664305gqr@gmail.com>
2022-05-16 19:03:44 +1200

    ArticleList, search function, css

--- speed/src/components/ArticleCard.js
+++ speed/src/components/ArticleCard.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../App.css';
+import BetterScroll from 'better-scroll';
 
 const ArticleCard = (props) => {
     const  article  = props.article;
-
+    new BetterScroll(".card-container")
     return(
         <div className="card-container">
-            <img src="https://commapress.co.uk/books/the-book-of-cairo/cairo-provisional-v3/image%2Fspan3" alt="" />
+            {/* <img src="https://commapress.co.uk/books/the-book-of-cairo/cairo-provisional-v3/image%2Fspan3" alt="" /> */}
             <div className="desc">
                 <h2>
                     <Link to={`/show-article/${article._id}`}>
--- speed/src/components/ShowArticleList.js
+++ speed/src/components/ShowArticleList.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import '../App.css';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import ArticleCard from './ArticleCard';
+import BetterScroll from 'better-scroll';
+// import ArticleCard from './ArticleCard';
 
 class ShowArticleList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        articles: []
+        articles: [],
+        backarticles: []
     };
   }
 
@@ -17,8 +19,10 @@ class ShowArticleList extends Component {
       .get('http://localhost:8082/api/articles')
       .then(res => {
         this.setState({
-            articles: res.data
+            articles: res.data,
+            backarticles: res.data
         })
+        new BetterScroll(".list")
       })
       .catch(err =>{
         console.log('Error from ShowArticleList');
@@ -28,43 +32,68 @@ class ShowArticleList extends Component {
 
   render() {
     const articles = this.state.articles;
-    console.log("PrintBook: " + articles);
-    let articleList;
+    // console.log("PrintBook: " + articles);
+    // let articleList;
 
-    if(!articles) {
-        articleList = "there is no article record!";
-    } else {
-        articleList = articles.map((article, k) =>
-        <ArticleCard article={article} key={k} />
-      );
-    }
+    // if(!articles) {
+    //     articleList = "there is no article record!";
+    // } else {
+    //     articleList = articles.map((article, k) =>
+    //     <ArticleCard article={article} key={k} />
+    //   );
+    // }
 
     return (
       <div className="ShowArticleList">
         <div className="container">
-          <div className="row">
-            <div className="col-md-12">
-              <br />
-              <h2 className="display-4 text-center">Articles List</h2>
+            <div className="row">
+                <div className="col-md-12">
+                <br />
+                <h2 className="display-4 text-center">Articles List</h2>
+                </div>
+
+                <div className="col-md-11">
+                <Link to="/create-article" className="btn btn-outline-dark float-left">
+                    + Add New Articles
+                </Link>
+                <br />
+                <br />
+                <hr />
+                </div>
+
             </div>
-
-            <div className="col-md-11">
-              <Link to="/create-article" className="btn btn-outline-warning float-right">
-                + Add New Articles
-              </Link>
-              <br />
-              <br />
-              <hr />
+            <input onInput={this.handleInput}/>
+            <div className="list" style={{height:'400px',width:"980px",backgroundColor:'#b5b5b5',overflow:'hidden'}}>
+                    {/* {articleList} */}
+                    <div className="content">
+                        {
+                        articles.map((article, index)=>
+                                <dl key={index}>
+                                    <dt>{article.title}</dt>
+                                    <dd>{article.author} </dd>
+                                </dl>
+                            )
+                        }
+                    </div>
             </div>
-
-          </div>
-
-          <div className="list">
-                {articleList}
-          </div>
+          <div>---</div>
+          {/* <hr/> */}
         </div>
       </div>
     );
+  }
+
+  handleInput = (event) => {
+    console.log("input",event.target.value)
+
+    var newList = this.state.backarticles.filter(item=>item.title.toUpperCase().includes(event.target.value.toUpperCase())
+    || item.author.toUpperCase().includes(event.target.value.toUpperCase()))
+
+    /* console.log(newList) */
+
+    this.setState({
+        articles:newList
+    })
   }
 }
 
